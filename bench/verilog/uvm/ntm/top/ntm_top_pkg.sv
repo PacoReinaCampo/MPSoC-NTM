9c0adeb275bbfc902c9a7875133b64b82ed33c06
PacoReinaCampo <frareicam@gmail.com>
2021-10-24 15:10:51 +0200

    Working in MPSoC-NTM

--- bench/verilog/uvm/ntm/top/ntm_top_pkg.sv
+++ bench/verilog/uvm/ntm/top/ntm_top_pkg.sv
@@ -37,7 +37,7 @@
 // Author(s):
 //   Paco Reina Campo <pacoreinacampo@queenfield.tech>
 
-module ntm_top_testbench;
+package ntm_top_pkg;
 
   ///////////////////////////////////////////////////////////////////////
   // Types
@@ -57,132 +57,8 @@ module ntm_top_testbench;
   parameter L=64;
   parameter R=64;
 
-  // FUNCTIONALITY
-  parameter STIMULUS_NTM_TOP_TEST   = 0;
-  parameter STIMULUS_NTM_TOP_CASE_0 = 0;
-  parameter STIMULUS_NTM_TOP_CASE_1 = 0;
-
   ///////////////////////////////////////////////////////////////////////
   // Signals
   ///////////////////////////////////////////////////////////////////////
 
-  // GLOBAL
-  wire CLK;
-  wire RST;
-
-  // TOP
-  // CONTROL
-  wire start_top;
-  wire ready_top;
-
-  wire w_in_l_enable_top;
-  wire w_in_x_enable_top;
-  wire k_in_i_enable_top;
-  wire k_in_l_enable_top;
-  wire k_in_k_enable_top;
-  wire b_in_enable_top;
-  wire x_in_enable_top;
-  wire y_out_enable_top;
-
-  // DATA
-  wire [DATA_SIZE-1:0] size_x_in_top;
-  wire [DATA_SIZE-1:0] size_y_in_top;
-  wire [DATA_SIZE-1:0] size_n_in_top;
-  wire [DATA_SIZE-1:0] size_w_in_top;
-  wire [DATA_SIZE-1:0] size_l_in_top;
-  wire [DATA_SIZE-1:0] size_r_in_top;
-  wire [DATA_SIZE-1:0] w_in_top;
-  wire [DATA_SIZE-1:0] k_in_top;
-  wire [DATA_SIZE-1:0] b_in_top;
-  wire [DATA_SIZE-1:0] x_in_top;
-  wire [DATA_SIZE-1:0] y_out_top;
-
-  ///////////////////////////////////////////////////////////////////////
-  // Body
-  ///////////////////////////////////////////////////////////////////////
-
-  ntm_top_stimulus #(
-    // SYSTEM-SIZE
-    .DATA_SIZE(DATA_SIZE),
-
-    .X(X),
-    .Y(Y),
-    .N(N),
-    .W(W),
-    .L(L),
-    .R(R),
-
-    // FUNCTIONALITY
-    .STIMULUS_NTM_TOP_TEST(STIMULUS_NTM_TOP_TEST),
-    .STIMULUS_NTM_TOP_CASE_0(STIMULUS_NTM_TOP_CASE_0),
-    .STIMULUS_NTM_TOP_CASE_1(STIMULUS_NTM_TOP_CASE_1)
-  )
-  top_stimulus(
-    // GLOBAL
-    .CLK(CLK),
-    .RST(RST),
-
-    // CONTROL
-    .NTM_TOP_START(start_top),
-    .NTM_TOP_READY(ready_top),
-
-    .NTM_TOP_W_IN_L_ENABLE(w_in_l_enable_top),
-    .NTM_TOP_W_IN_X_ENABLE(w_in_x_enable_top),
-    .NTM_TOP_K_IN_I_ENABLE(k_in_i_enable_top),
-    .NTM_TOP_K_IN_L_ENABLE(k_in_l_enable_top),
-    .NTM_TOP_K_IN_K_ENABLE(k_in_k_enable_top),
-    .NTM_TOP_B_IN_ENABLE(b_in_enable_top),
-    .NTM_TOP_X_IN_ENABLE(x_in_enable_top),
-    .NTM_TOP_Y_OUT_ENABLE(y_out_enable_top),
-
-    // DATA
-    .NTM_TOP_SIZE_X_IN(size_x_in_top),
-    .NTM_TOP_SIZE_Y_IN(size_y_in_top),
-    .NTM_TOP_SIZE_N_IN(size_n_in_top),
-    .NTM_TOP_SIZE_W_IN(size_w_in_top),
-    .NTM_TOP_SIZE_L_IN(size_l_in_top),
-    .NTM_TOP_SIZE_R_IN(size_r_in_top),
-    .NTM_TOP_W_IN(w_in_top),
-    .NTM_TOP_K_IN(k_in_top),
-    .NTM_TOP_B_IN(b_in_top),
-    .NTM_TOP_X_IN(x_in_top),
-    .NTM_TOP_Y_OUT(y_out_top)
-  );
-
-  // TOP
-  ntm_top #(
-    .DATA_SIZE(DATA_SIZE)
-  )
-  top(
-    // GLOBAL
-    .CLK(CLK),
-    .RST(RST),
-
-    // CONTROL
-    .START(start_top),
-    .READY(ready_top),
-
-    .W_IN_L_ENABLE(w_in_l_enable_top),
-    .W_IN_X_ENABLE(w_in_x_enable_top),
-    .K_IN_I_ENABLE(k_in_i_enable_top),
-    .K_IN_L_ENABLE(k_in_l_enable_top),
-    .K_IN_K_ENABLE(k_in_k_enable_top),
-    .B_IN_ENABLE(b_in_enable_top),
-    .X_IN_ENABLE(x_in_enable_top),
-    .Y_OUT_ENABLE(y_out_enable_top),
-
-    // DATA
-    .SIZE_X_IN(size_x_in_top),
-    .SIZE_Y_IN(size_y_in_top),
-    .SIZE_N_IN(size_n_in_top),
-    .SIZE_W_IN(size_w_in_top),
-    .SIZE_L_IN(size_l_in_top),
-    .SIZE_R_IN(size_r_in_top),
-    .W_IN(w_in_top),
-    .K_IN(k_in_top),
-    .B_IN(b_in_top),
-    .X_IN(x_in_top),
-    .Y_OUT(y_out_top)
-  );
-
-endmodule
+endpackage
