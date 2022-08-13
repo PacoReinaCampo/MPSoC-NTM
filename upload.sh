defa042b94d6ca00dd4861b3092e3c51ace958dc
PacoReinaCampo <frareicam@gmail.com>
2022-08-13 12:43:32 +0200

    Working in MPSoC-NTM 🦀

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in MPSoC-NTM"
+git commit -m "Working in MPSoC-NTM 🦀"
 git push origin master
