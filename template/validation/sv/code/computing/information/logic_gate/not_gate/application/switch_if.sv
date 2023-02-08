ad29ee3642fe5c5aa74194b612f29eef09f266ef
PacoReinaCampo <frareicam@gmail.com>
2023-02-08 15:58:14 +0100

    Working in MPSoC-NTM 🦀

---
+++ template/validation/sv/code/computing/information/logic_gate/not_gate/application/switch_if.sv
@@ -0,0 +1,13 @@
+interface switch_if (input bit clk);
+  logic rstn;
+  logic vld;
+
+  logic [ 7:0] addr;
+  logic [15:0] data;
+  
+  logic [ 7:0] addr_a;
+  logic [15:0] data_a;
+  
+  logic [ 7:0] addr_b;
+  logic [15:0] data_b;
+endinterface
