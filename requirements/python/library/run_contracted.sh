302a693214f2b6db800de128e40a5ccb80489a91
PacoReinaCampo <frareicam@gmail.com>
2023-05-09 18:18:50 +0200

    Working in MPSoC-NTM 🦀

--- requirements/python/library/run_contracted.sh
+++ requirements/python/library/run_contracted.sh
@@ -46,6 +46,7 @@ python3 arithmetic/test_matrix_arithmetic.py
 python3 arithmetic/test_scalar_arithmetic.py
 python3 arithmetic/test_tensor_arithmetic.py
 python3 arithmetic/test_vector_arithmetic.py
+
 python3 math/algebra/test_matrix_algebra.py
 python3 math/algebra/test_scalar_algebra.py
 python3 math/algebra/test_tensor_algebra.py
@@ -59,3 +60,48 @@ python3 math/function/test_vector_function.py
 python3 math/statitics/test_matrix_statitics.py
 python3 math/statitics/test_scalar_statitics.py
 python3 math/statitics/test_vector_statitics.py
+
+python3 ntm/memory/test_addressing_test.py
+python3 ntm/memory/test_matrix_content_based_addressing_test.py
+python3 ntm/memory/test_vector_content_based_addressing_test.py
+python3 ntm/read_heads/test_reading_test.py
+python3 ntm/top/test_interface_matrix_test.py
+python3 ntm/top/test_interface_top_test.py
+python3 ntm/top/test_interface_vector_test.py
+python3 ntm/top/test_output_vector_test.py
+python3 ntm/top/test_top_test.py
+python3 ntm/trained/test_trained_top_test.py
+python3 ntm/write_heads/test_erasing_test.py
+python3 ntm/write_heads/test_writing_test.py
+
+python3 dnc/memory/dnc_addressing_test.py
+python3 dnc/memory/dnc_allocation_weighting_test.py
+python3 dnc/memory/dnc_backward_weighting_test.py
+python3 dnc/memory/dnc_forward_weighting_test.py
+python3 dnc/memory/dnc_matrix_content_based_addressing_test.py
+python3 dnc/memory/dnc_memory_matrix_test.py
+python3 dnc/memory/dnc_memory_retention_vector_test.py
+python3 dnc/memory/dnc_precedence_weighting_test.py
+python3 dnc/memory/dnc_read_content_weighting_test.py
+python3 dnc/memory/dnc_read_vectors_test.py
+python3 dnc/memory/dnc_read_weighting_test.py
+python3 dnc/memory/dnc_sort_vector_test.py
+python3 dnc/memory/dnc_temporal_link_matrix_test.py
+python3 dnc/memory/dnc_usage_vector_test.py
+python3 dnc/memory/dnc_vector_content_based_addressing_test.py
+python3 dnc/memory/dnc_write_content_weighting_test.py
+python3 dnc/memory/dnc_write_weighting_test.py
+python3 dnc/top/dnc_interface_matrix_test.py
+python3 dnc/top/dnc_interface_top_test.py
+python3 dnc/top/dnc_interface_vector_test.py
+python3 dnc/top/dnc_output_vector_test.py
+python3 dnc/top/dnc_top_test.py
+python3 dnc/trained/dnc_trained_top_test.py
+
+python3 trainer/differentiation/test_matrix_controller_differentiation_test.py
+python3 trainer/differentiation/test_vector_controller_differentiation_test.py
+python3 trainer/FNN/test_fnn_trainer_test.py
+python3 trainer/LSTM/activation/test_lstm_activation_trainer_test.py
+python3 trainer/LSTM/forget/test_lstm_forget_trainer_test.py
+python3 trainer/LSTM/input/test_lstm_input_trainer_test.py
+python3 trainer/LSTM/output/test_lstm_output_trainer_test.py
