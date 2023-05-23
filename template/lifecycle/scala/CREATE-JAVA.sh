aa85024d969abe26afd6ce46d6b04668cdaa4a1a
PacoReinaCampo <frareicam@gmail.com>
2023-05-23 14:26:49 +0200

    Working in MPSoC-NTM 🦀

---
+++ template/lifecycle/scala/CREATE-JAVA.sh
@@ -0,0 +1,88 @@
+###################################################################################
+##                                            __ _      _     _                  ##
+##                                           / _(_)    | |   | |                 ##
+##                __ _ _   _  ___  ___ _ __ | |_ _  ___| | __| |                 ##
+##               / _` | | | |/ _ \/ _ \ '_ \|  _| |/ _ \ |/ _` |                 ##
+##              | (_| | |_| |  __/  __/ | | | | | |  __/ | (_| |                 ##
+##               \__, |\__,_|\___|\___|_| |_|_| |_|\___|_|\__,_|                 ##
+##                  | |                                                          ##
+##                  |_|                                                          ##
+##                                                                               ##
+##                                                                               ##
+##              QueenField                                                       ##
+##              Multi-Processor System on Chip                                   ##
+##                                                                               ##
+###################################################################################
+
+###################################################################################
+##                                                                               ##
+## Copyright (c) 2022-2025 by the author(s)                                      ##
+##                                                                               ##
+## Permission is hereby granted, free of charge, to any person obtaining a copy  ##
+## of this software and associated documentation files (the "Software"), to deal ##
+## in the Software without restriction, including without limitation the rights  ##
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     ##
+## copies of the Software, and to permit persons to whom the Software is         ##
+## furnished to do so, subject to the following conditions:                      ##
+##                                                                               ##
+## The above copyright notice and this permission notice shall be included in    ##
+## all copies or substantial portions of the Software.                           ##
+##                                                                               ##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    ##
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      ##
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   ##
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        ##
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, ##
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     ##
+## THE SOFTWARE.                                                                 ##
+##                                                                               ##
+## ============================================================================= ##
+## Author(s):                                                                    ##
+##   Paco Reina Campo <pacoreinacampo@queenfield.tech>                           ##
+##                                                                               ##
+###################################################################################
+
+touch code/classes/philosophers/ntm_philosophers.scala
+touch code/classes/soldiers/ntm_soldiers.scala
+touch code/classes/workers/ntm_workers.scala
+touch code/computing/advanced_computer_architecture/multi_processor_system_on_chip/multi_processor_system_on_chip.scala
+touch code/computing/advanced_computer_architecture/processing_unit/mimd.scala
+touch code/computing/advanced_computer_architecture/processing_unit/misd.scala
+touch code/computing/advanced_computer_architecture/processing_unit/simd.scala
+touch code/computing/advanced_computer_architecture/processing_unit/sisd.scala
+touch code/computing/advanced_computer_architecture/system_on_chip/bus_on_chip.scala
+touch code/computing/advanced_computer_architecture/system_on_chip/network_on_chip.scala
+touch code/computing/computer_architecture/harvard_architecture/pu_riscv.scala
+touch code/computing/computer_architecture/harvard_architecture/pu_or1k.scala
+touch code/computing/computer_architecture/von_neumann_architecture/pu_riscv.scala
+touch code/computing/computer_architecture/von_neumann_architecture/pu_msp430.scala
+touch code/computing/information/bit/bit.scala
+touch code/computing/information/combinational_logic/arithmetic_circuits.scala
+touch code/computing/information/combinational_logic/logic_circuits.scala
+touch code/computing/information/finite_state_machine/finite_state_machine.scala
+touch code/computing/information/logic_gate/and_gate.scala
+touch code/computing/information/logic_gate/nand_gate.scala
+touch code/computing/information/logic_gate/nor_gate.scala
+touch code/computing/information/logic_gate/not_gate.scala
+touch code/computing/information/logic_gate/or_gate.scala
+touch code/computing/information/logic_gate/xnor_gate.scala
+touch code/computing/information/logic_gate/xor_gate.scala
+touch code/computing/information/logic_gate/yes_gate.scala
+touch code/computing/information/pushdown_automaton/pushdown_automaton.scala
+touch code/computing/neural_network/feedforward_neural_network/feedforward_neural_network.scala
+touch code/computing/neural_network/long_short_term_memory_neural_network/long_short_term_memory_neural_network.scala
+touch code/computing/neural_network/transformer_neural_network/transformer_neural_network.scala
+touch code/computing/turing_machine/differentiable_neural_computer/feedforward_differentiable_neural_computer.scala
+touch code/computing/turing_machine/differentiable_neural_computer/lstm_differentiable_neural_computer.scala
+touch code/computing/turing_machine/differentiable_neural_computer/transformer_differentiable_neural_computer.scala
+touch code/computing/turing_machine/neural_turing_machine/feedforward_neural_turing_machine.scala
+touch code/computing/turing_machine/neural_turing_machine/lstm_neural_turing_machine.scala
+touch code/computing/turing_machine/neural_turing_machine/transformer_neural_turing_machine.scala
+touch code/pkg/classes/ntm_philosophers_pkg.scala
+touch code/pkg/classes/ntm_soldiers_pkg.scala
+touch code/pkg/classes/ntm_workers_pkg.scala
+touch code/pkg/ieee/math_complex-body.scala
+touch code/pkg/ieee/math_complex.scala
+touch code/pkg/ieee/math_real-body.scala
+touch code/pkg/ieee/math_real.scala
+touch code/pkg/computing/state_pkg.c
